67783044d34b6b838c362bbf9b45fec0701889e0
parvej0ali <83010920+Parvej0Ali@users.noreply.github.com>
2024-03-11 15:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const purchaseRoutes = require("./routes/PurchaseRoutes");
 
 // Middleware
 const corsOption={
-  origin:"https://claimmanagementparvej.netlify.app",
+  origin:"http://localhost:5173",
   method:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials:true,
 };

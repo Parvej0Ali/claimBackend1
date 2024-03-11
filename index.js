5b18462908b5e1660e3489345ff3ec866b1af757
parvej0ali <83010920+Parvej0Ali@users.noreply.github.com>
2024-03-12 01:45:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,15 @@ const authRoutes = require("./routes/AuthRoutes");
 const claimRoutes = require("./routes/ClaimRoutes");
 const purchaseRoutes = require("./routes/PurchaseRoutes");
 
-// Middleware
+
+//cross origin resource sharing
 const corsOption={
   origin:"http://localhost:5173",
   method:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials:true,
 };
 app.use(cors(corsOption));
-
+// Middleware
 app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api/policies", policyRoutes);
@@ -30,10 +31,10 @@ app.use("/api/purchase", purchaseRoutes);
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Welcome to the Claims Management System API");
+  res.send("Hello, this is claim management system backend");
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
